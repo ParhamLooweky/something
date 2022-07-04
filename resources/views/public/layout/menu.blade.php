bb2b8e66893a284882ef9487e071c85234d4decb
ParhamLooweky <bass28961@gmail.com>
2022-07-04 14:40:36 +0430

    Checkout commit

--- resources/views/public/layout/menu.blade.php
+++ resources/views/public/layout/menu.blade.php
@@ -1,3 +1,31 @@
+<div class="top-bar">
+    <div class="container">
+        <div class="top top-left">
+            <div class="sidebar-widget text-left small-screen-center widget_text"> Opening Hours: 09:00 - 17:00 <strong>Monday</strong> to <strong>Friday</strong> </div>
+        </div>
+        <div class="top top-right">
+            <div class="sidebar-widget text-right small-screen-center widget_nav_menu" id="nav_menu-5">
+                <div class="menu-top-bar-menu-container">
+                    <ul class="menu">
+                        <li> <a href="{{route('billing')}}">Checkout</a> </li>
+                        <li> <a href="{{route('terms')}}">Terms</a> </li>
+                        <li> <a href="{{route('faq')}}">FAQ</a> </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="sidebar-widget text-right small-screen-center widget_search">
+                <form action="shop-index.html" method="get" name="searchform">
+                    <div class="input-group">
+                        <input class="form-control" id="s" name="s" placeholder="Search" type="text"> <span class="input-group-btn">
+                            <button class="btn btn-primary" type="submit" id="searchsubmit" value="Search">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </span> </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="menu navbar navbar-static-top header-logo-center-menu-below oxy-mega-menu text-caps" id="masthead">
     <div class="logo-navbar container-logo">
         <div class="container">
